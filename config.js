cb77c99c07e5125c155a2668c419e05d5c4fc477
I_Love_My_Crush <phieulang1908@gmail.com>
2021-06-21 08:19:21 +0700

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://ilovemycrush1908:OkuLNa8Cl87Qb5QF@cluster0.etdi8.mongodb.net/mydb?retryWrites=true&w=majority',
+    SECRET_KEY: 'Some very secret key'
+}
